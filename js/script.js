91cac1f039824df8573c35f3327e9ef76169b0d8
caiocroccia22 <caiocroccia22@gmail.com>
2024-03-31 17:52:44 -0300

    conserto do metodo addDigit e criação do processOperation

--- js/script.js
+++ js/script.js
@@ -5,30 +5,59 @@ const buttons = document.querySelectorAll("#buttons-container button");
 
 // Classe calculadora
 class Calculator {
-  constructor(previousOperationText, currentOperationText){
+  constructor(previousOperationText, currentOperationText) {
     this.previousOperationText = previousOperationText;
     this.currentOperationText = currentOperationText;
-    this.currentOperation = ""
+    this.currentOperation = "";
   }
 
 
   // Declaração de metodos
 
   // adicionar o valor digitado (digit) para o calculator screen
-  addDigit(digit){
+  addDigit(digit) {
     // Verificar se a operação atual tem um ponto
-    if (digit === "." && this.currentOperationText.innerText.includes(".")){
-    this.currentOperation = digit
-    this.updateScreen()
+    if (digit === "." && this.currentOperationText.innerText.includes(".")) {
+     return;
     }
+    this.currentOperation = digit;
+    this.updateScreen();
+  }
+
+  // Metodo para executar todas as operações da calculadora
+  processOperation(operation) {
+
+    // Aqui foi pego os dois valores e passado para conversão numérica
+    let operationValue
+    let previous = +this.previousOperationText.innerText;
+    let current = +this.currentOperationText.innerText;
+
+
+    switch(operation){
+
+      case "+":
+        operation = previous + current;
+        this.updateScreen(operationValue, operation, current, previous);
+        break;
+      default:
+        return;
+
+    }
+
+
+
   }
 
 
-
   // Troca os valores do calculator screen
-  updateScreen(){
+  updateScreen(operation = null, 
+              operationValue = null, 
+              current = null, 
+              previous = null) 
+              {
+    console.log(operationValue, current, previous, operation);
     // Numeros que a gente digite aparece no layout
-    this.currentOperationText.innerText += this.currentOperation
+    this.currentOperationText.innerText += this.currentOperation;
   }
 
 }
@@ -43,12 +72,13 @@ buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const value = e.target.innerText;
     // console.log(value); -> teste do click nos numeros
+
     // Operador + converte o valor que eu recebeer em numero
-    if(+value >= 0 || value === "." ){
+    if (+value >= 0 || value === ".") {
       calc.addDigit(value);
-    }else{
+    } else {
       // Se não conseguir fazer a conversão para numero, significa que é uma operação
-      console.log("Op: " + value);
+      calc.processOperation("Op: " + value);
     }
 
 
